dcf351f85e880148d1cdfadf7ba202736c87e8d5
jazzyzoney <jaqtash22@gmail.com>
2025-09-18 07:40:44 +0000

    greekgods

--- 03._Greek_Gods_Rest_API/app.js
+++ 03._Greek_Gods_Rest_API/app.js
@@ -14,6 +14,18 @@ const greekGods = [
     }
 ]
 
+//let lastId = 2
+
+let nextId = 3
+
+// postfix notation 2, 3
+// console.log(lastId++)
+// console.log(lastId)
+
+// prefix notation 3, 3
+// console.log(++lastId)
+// console.log(lastId)
+
 //route (handler)
 app.get("/greekgods", (req, res) => { //2 arguments, the endpoint and the callback function 
     res.send({ data: greekGods }) // vi sender json og ikke string - det er der mange grunde til (1 grund, så kan andre servere parse det )
@@ -31,6 +43,67 @@ app.get("/greekgods/:id", (req, res) => {
 })
 
 // design resten af API'et - udfordringen er hvordan man vil håndtere Id
+
+
+
+app.post("/greekgods", (req, res) => { //
+        if(!req.body){
+            return res.status(400).send({errorMessage:"requires a JSON body"})
+        }
+
+        const newGreekGod =  req.body
+        //req.body.id = lastId++ // undefined.id, missing a body parser
+        req.body.id = nextId++
+        greekGods.push(newGreekGod)
+        
+        res.send({ data: newGreekGod })
+})
+
+// spread operator ... removes the content from an array and spreads it into other arrays 
+// const bicycle = {
+//     yawn: true,
+// } 
+
+// const bicycle = {
+//     yawn: false
+// } // will return false
+// const test = {...bicycleOne}
+// console.log({ ...bicycleOne, ...bicycleTwo })
+
+app.patch("/greekgods/:id", (req,res) => {
+    const providedGreekGodId = Number(req.params.id)
+    const foundGreekGodIndex = greekGods.find((greekGod) => greekGod.id === providedGreekGodId)
+
+    if (!foundGreekGodIndex === -1) {
+        returnres.status(404).send({ errorMessage: `Greek God not found by id ${providedGreekGodId}`  })
+    }
+
+    const foundGreekGod = greekGods[foundGreekGodIndex]
+    const newGreekGod = {...foundGreekGod, ...req.body, id: foundGreekGod.id}
+
+    greekGods[foundGreekGodIndex]=newGreekGod
+
+    res.send({ data: newGreekGod})
+})
+
+app.delete("/greekgods/:id", (req,res) => {
+    // filter is a bad solution here
+    const providedGreekGodId = Number(req.params.id)
+    const foundGreekGod = greekGods.find((greekGod) => greekGod.id === providedGreekGodId)
+
+    if (!foundGreekGodIndex === -1) {
+        return res.status(400).send({ errorMessage: `Greek God not found by id ${providedGreekGodId}` })
+    } else {
+        greekGods.splice(foundGreekGodIndex, 1)
+        
+        res.status(204).send()
+    }
+})
+
+
+// 2xx - success
+// 4xx - client failed
+// 5xx - server failed
 
 
 //task: app.listen can be given a callback function in the second argument. console.log something in it 
