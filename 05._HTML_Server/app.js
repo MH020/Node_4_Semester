9604afbe53176176df2df07dbfdfd282b043ccfd
jazzyzoney <jaqtash22@gmail.com>
2025-09-25 09:05:01 +0000

    end of 05

--- 05._HTML_Server/app.js
+++ 05._HTML_Server/app.js
@@ -3,9 +3,9 @@ const app = express()
 
 import path from "path"
 
-// app.get("/", (req,res) => {
-//     res.sendFile(__dirname + "/public/frontend/index.html")
-// })
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve("public/frontend/index.html"))
+})
 
 app.use(express.static("public")) //det vil give adgang til hele public mappen i stedet for at man skal give specifik path hver gang man skal hive fat i en fil fra den mappe
 // sikkerhedsfeature
@@ -29,6 +29,10 @@ let visitorsCount = 0
 
 app.get("/visitorscount", (req,res) => {
     res.send({ data: ++visitorsCount })
+})
+
+app.get("/visitorsdoorway", (req,res) => { // the user will be redirected to visitorscount 
+    res.redirect("/visitorscount") // redirection in frontend means the browser will take u to different pages, but in the backend it means it will redirect u to another route 
 })
 
 const PORT = 8080
